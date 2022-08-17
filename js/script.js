ec26548d98d89c522aa7c2789be6c54d3c3b990a
k12re <83732570+k12re@users.noreply.github.com>
2022-08-17 15:35:15 +0200

    tried the questions and reviewed and corrected in one go

---
+++ js/script.js
@@ -0,0 +1,61 @@
+//Question 1
+
+function receivingFunction(argument) {
+  if (typeof argument === "function") {
+    argument();
+  } else {
+    console.log("The argument is not a function");
+  }
+}
+
+const callbackFunction = function () {
+  console.log("I am a callback function");
+};
+
+receivingFunction(callbackFunction);
+
+//Question 2
+
+const prizes = ["Bag of bananas", "Box of frogs", "Car of clowns"];
+
+prizes.forEach(function (prize, index) {
+  console.log(`Prize ${index + 1}: ${prize}`);
+});
+
+//Question 3
+
+//My solution, probaly not a good solution
+
+//let milliseconds = 5000;
+
+//setTimeout(function () {
+//  console.log("I waited 5 seconds before executing");
+//}, milliseconds);
+
+//OR
+
+const seconds = 5;
+
+function logMessage() {
+  console.log(`I waited ${seconds} seconds before executing`);
+}
+
+setTimeout(logMessage, seconds * 1000);
+
+//Question 4
+
+const interval = 500;
+let counter = 0;
+const limit = 4;
+
+function logNumber() {
+  counter++;
+
+  console.log(counter);
+
+  if (counter === limit) {
+    clearInterval(intervalId);
+  }
+}
+
+const intervalId = setInterval(logNumber, interval);
